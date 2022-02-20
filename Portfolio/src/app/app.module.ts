d90bec099552e07fe98888326e3ac651255d3446
MattAlvero <matthewadrianoalvero@gmail.com>
2022-02-19 23:38:20 -0600

    Initial commit

---
+++ Portfolio/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarTopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
